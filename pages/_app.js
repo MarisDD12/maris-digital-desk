e5b602e9395e17d64f29f5ac786f1c1c1783bff5
MarisDD12 <bymaricraft@gmail.com>
2025-05-22 17:17:34 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
-import '@/styles/globals.css'; // ✅ This pulls in Tailwind CSS
+import '../styles/globals.css';
+ // ✅ This pulls in Tailwind CSS
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
