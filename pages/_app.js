9141148180aea482dc1dc75eb9f0994858e4a1aa
MarisDD12 <bymaricraft@gmail.com>
2025-06-17 15:37:09 -0400

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-import '../styles/globals.css';
- // ✅ This pulls in Tailwind CSS
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
--- pages/thank-you.js
+++
@@ -1,8 +0,0 @@
-export default function ThankYou() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center p-6">
-      <h1 className="text-3xl font-bold">Thank You!</h1>
-      <p className="mt-2">Your purchase was successful. You can now access your widget below.</p>
-    </div>
-  );
-}
